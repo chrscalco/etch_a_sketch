5712d23de71d06e1f0cea3f3668990333a139a6c
chrscalco <xrscalco@gmail.com>
2024-12-22 04:52:10 +0200

    Completed the assignment.

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 const container = document.querySelector("#container");
 const button = document.querySelector("#buttonStart");
-
 const buttonStart = document.createElement("button");
 
 buttonStart.classList.add("buttonStart");
 buttonStart.textContent = "Start Painting!";
 button.appendChild(buttonStart);
 
+buttonStart.addEventListener("click", () => {
+
+    deleteGrid();
+    const size = prompt("Enter the grid size you want between 10 and 100.")
+
+    while(size<10 || size>100) {
+        size = prompt("Choose a size between 10 and 100.")
+    }
+
+    container.setAttribute("style","border: 2px solid black");
+    createGrid(size,size);
+
+    const gridCellHover = document.querySelectorAll(".gridCellCol");
+
+    gridCellHover.forEach(function(e) {
+        e.addEventListener("mouseover", () => {
+            e.setAttribute("style", "background-color: red;")
+        })
+    });
+})
+
+
 function createGrid(rows, cols) {
-    for (i=0; i <= rows; i++) {
+    for (i=1; i <= rows; i++) {
         const gridCellRow = document.createElement("div");
         gridCellRow.classList.add("gridCellRow");
         container.appendChild(gridCellRow);
-        for (j=0; j<=cols; j++) {
+        for (j=1; j<=cols; j++) {
             const gridCellCol = document.createElement("div")
             gridCellCol.classList.add("gridCellCol");
             gridCellRow.appendChild(gridCellCol);
@@ -20,14 +41,13 @@ function createGrid(rows, cols) {
     }
 
 }
-let cols = 32;
-let rows = 32;
-createGrid(rows,cols);
 
-const gridCellHover = document.querySelectorAll(".gridCellCol");
+function deleteGrid() {
+    const removeGrid = document.getElementById("container");
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
 
-gridCellHover.forEach(function(e) {
-    e.addEventListener("mouseover", () => {
-        e.setAttribute("style", "background-color: red;")
-    })
-});
+
+
